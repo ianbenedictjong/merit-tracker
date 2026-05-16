927d1e4527d441fb7db9e225a19185f02bff0831
Ian <ianbenedict2011@gmail.com>
2026-05-16 15:16:22 +0900

    initial authentication page setup

--- client.js
+++ client.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export { supabase }
+// Add a quick console.log here to debug if it fails again
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Supabase env variables are missing!")
+}
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
